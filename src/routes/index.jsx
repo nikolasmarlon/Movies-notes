8d4b8a59c7ac4d18a3eb08a9a3a9e92c113ea96a
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2024-01-03 00:03:04 -0300

    Verificando rota e redirecionando

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,8 @@
 
 import { BrowserRouter } from "react-router-dom";
 
+// do contexto
+import { useAuth } from "../hooks/auth";
 
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
@@ -9,9 +11,11 @@ import { AuthRoutes } from "./auth.routes";
 
 
 export function Rotas() {
+
+    const { user } = useAuth()
     return(
         <BrowserRouter>
-            <AuthRoutes />
+           { user ? <AppRoutes /> : <AuthRoutes />}
         </BrowserRouter>
     )
 }
