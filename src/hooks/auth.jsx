f109e3076ab24563a4d2a3bf225f2e30241c688e
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2024-01-02 23:56:21 -0300

    Função de autenticação

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,10 +1,38 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+
+import { api } from "../services/api";
 
 export const AuthContext = createContext({})
 
 function AuthProvider({children}){
+
+    const [data, setData] = useState({})
+
+
+    async function login({ email, password }){
+        // mandar para o backend
+
+
+       try {
+            const response = await api.post('/sessions', { email, password})
+
+            const { user, token } = response.data
+
+            api.defaults.headers.authorization = `Bearer ${token}`
+            setData({ user, token})
+
+
+       } catch (error) {
+            if(error.response){
+                alert(error.response.data.message)
+            } else{
+                alert("Não foi possível entrar!")
+            }
+       }
+
+    }
     return(
-        <AuthContext.Provider value={{ email: 'nikolas@gmail.com'}}>
+        <AuthContext.Provider value={{ login, user: data.user }}>
             {children}
         </AuthContext.Provider>
     )
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,6 +2,8 @@ import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { Link } from "react-router-dom";
 
+import { useState } from "react";
+
 import { useAuth } from "../../hooks/auth";
 
 
@@ -11,10 +13,16 @@ import { FiLock, FiMail } from 'react-icons/fi'
 
 export function Login(){
 
+    const [ email, setEmail ] = useState("")
+    const [ password, setPassword ] = useState("")
 
-    const dadosDoContexto = useAuth()
+    // desestruturação contexto 
+    const { login }= useAuth()  
 
-    
+    // Lidar com login(do contexto)
+    function handleLogin(){
+        login({ email, password})
+    }
    
 
     return(
@@ -24,12 +32,10 @@ export function Login(){
                 <p>Aplicação para acompanhar tudo que assistir.</p>
 
                 <h2>Faça seu login</h2>
-                <Input required type="text" icon={FiMail} placeholder="E-mail" />
-                <Input required type="password" icon={FiLock} placeholder="Senha" />
+                <Input required type="text" icon={FiMail} placeholder="E-mail" onChange={ evento => setEmail(evento.target.value)} />
+                <Input required type="password" icon={FiLock} placeholder="Senha"  onChange={ e => setPassword(e.target.value)} />                
 
-                <p>{dadosDoContexto.email}</p>
-
-                <Button isNew title="Entrar" />
+                <Button isNew title="Entrar" onClick={handleLogin} />
 
                 <Link to="/register">Criar conta</Link>
             </Form>
