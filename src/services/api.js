817117fd5beb828cc06f499ac0f965fd167437d0
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2024-01-30 20:24:45 -0300

    link api remoto

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,6 @@ import axios from "axios";
 
 export const api = axios.create({
     // objeto de configurações
-    baseURL: "http://localhost:3333", // incluir a url que se repete na api(backend)
+    baseURL: "https://service-api-filmes.onrender.com", // incluir a url que se repete na api(backend)
     
 })
